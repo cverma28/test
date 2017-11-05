621902e98e5411b71cf0b1bedebacbb551d841b5
chana\verma <cverma28@gmail.com>
2017-11-05 19:49:06 +0530

    test

--- src/test/java/pack/Test2/Test123.java
+++ src/test/java/pack/Test2/Test123.java
@@ -1,6 +1,6 @@
 package pack.Test2;
 
- import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
@@ -10,8 +10,12 @@ public class Test123 {
 	WebDriver driver;
 
 	@Test
-	public void open() throws InterruptedException {for(int i=0;i<5;i++) {
-	System.out.println("Count is "+(i+1));	
-	}
+	public void open() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//Driver//chromedriver.exe");
+		driver = new ChromeDriver();
+driver.get("https:www.google.com");		
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Count is " + (i + 1));
+		}
 	}
 }
