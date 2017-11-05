d2e5b2b734bcd30e751a11f384830fc3fbda2309
chana\verma <cverma28@gmail.com>
2017-11-05 17:05:21 +0530

    First programe

---
+++ src/test/java/pack/Test2/Test123.java
@@ -0,0 +1,18 @@
+package pack.Test2;
+
+ import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Test123 {
+
+	WebDriver driver;
+
+	@Test
+	public void open() throws InterruptedException {for(int i=0;i<5;i++) {
+	System.out.println("Count"+i);	
+	
+	}
+	}
+}
